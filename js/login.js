71cadf2375a4609c2ca8e6621252083c09c99c09
unknown <ekramhossen60543@gmail.com>
2025-04-22 21:26:48 +0600

    added user validation

--- js/login.js
+++ js/login.js
@@ -1,4 +1,5 @@
 // search: form submit reloading the page
+// search: js onclick go to other html file
 
 // step-1: set event handler
 document
@@ -7,9 +8,18 @@ document
     // step-2: prevent default behavior (prevent reloading browser)
     event.preventDefault(); // Off browser reloading for html form tag
 
-    // step-3: get the phone number
+    // step-3: get the phone number and pin number
     const phoneNumber = document.getElementById("phone-number").value;
-    // step-4: get the pin number
     const pinNumber = document.getElementById("pin-number").value;
+
     console.log(phoneNumber, pinNumber);
+
+    // this is temporary. you should not do like this
+    // step: validate phone and pin
+    if (phoneNumber === "01718256229" && pinNumber === "12345") {
+      // step-5: allow user to use the website
+      window.location.href = "home.html";
+    } else {
+      alert("Wrong phone number or pin");
+    }
   });
